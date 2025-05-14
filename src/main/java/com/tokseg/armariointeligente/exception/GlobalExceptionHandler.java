2d400cde93d5b10ed24e12957ab7291faa543b90
Jean Meneses Barros <jeanmeneses@hotmail.com>
2025-05-13 22:00:29 -0300

    fix: melhora tratamento de exceções globais
    Melhora o tratamento de exceções globais para fornecer respostas de erro mais informativas. Adiciona tratamento para exceções de validação, retornando mensagens de erro detalhadas com erros específicos por campo. Além disso, remove o prefixo "uri=" da descrição da requisição em todas as respostas de erro para uma saída mais limpa.

--- src/main/java/com/tokseg/armariointeligente/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tokseg/armariointeligente/exception/GlobalExceptionHandler.java
@@ -1,9 +1,10 @@
 package com.tokseg.armariointeligente.exception;
 
 import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -12,32 +13,58 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorResponse errorDetails = new ErrorResponse(new Date(), HttpStatus.NOT_FOUND.value(), "Não encontrado", ex.getMessage(), request.getDescription(false));
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex,
+            WebRequest request) {
+        ErrorResponse errorDetails =
+                new ErrorResponse(new Date(), HttpStatus.NOT_FOUND.value(), "Não encontrado",
+                        ex.getMessage(), request.getDescription(false).replace("uri=", ""));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
-    public ResponseEntity<?> resourceAlreadyExistsException(ResourceAlreadyExistsException ex, WebRequest request) {
-        ErrorResponse errorDetails = new ErrorResponse(new Date(), HttpStatus.CONFLICT.value(), "Conflito", ex.getMessage(), request.getDescription(false));
+    public ResponseEntity<?> resourceAlreadyExistsException(ResourceAlreadyExistsException ex,
+            WebRequest request) {
+        ErrorResponse errorDetails = new ErrorResponse(new Date(), HttpStatus.CONFLICT.value(),
+                "Conflito", ex.getMessage(), request.getDescription(false).replace("uri=", ""));
         return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> badRequestException(BadRequestException ex, WebRequest request) {
-        ErrorResponse errorDetails = new ErrorResponse(new Date(), HttpStatus.BAD_REQUEST.value(), "Requisição inválida", ex.getMessage(), request.getDescription(false));
+        ErrorResponse errorDetails =
+                new ErrorResponse(new Date(), HttpStatus.BAD_REQUEST.value(), "Requisição inválida",
+                        ex.getMessage(), request.getDescription(false).replace("uri=", ""));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RelatedResourceException.class)
-    public ResponseEntity<?> relatedResourceException(RelatedResourceException ex, WebRequest request) {
-        ErrorResponse errorDetails = new ErrorResponse(new Date(), HttpStatus.CONFLICT.value(), "Conflito", ex.getMessage(), request.getDescription(false));
+    public ResponseEntity<?> relatedResourceException(RelatedResourceException ex,
+            WebRequest request) {
+        ErrorResponse errorDetails = new ErrorResponse(new Date(), HttpStatus.CONFLICT.value(),
+                "Conflito", ex.getMessage(), request.getDescription(false).replace("uri=", ""));
         return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex,
+            WebRequest request) {
+        ValidationErrorResponse errorResponse =
+                new ValidationErrorResponse(new Date(), HttpStatus.BAD_REQUEST.value(),
+                        "Erro de validação", "Verifique os campos informados",
+                        request.getDescription(false).replace("uri=", ""));
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errorResponse.addError(error.getField(), error.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
-        ErrorResponse errorDetails = new ErrorResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro interno do servidor", ex.getMessage(), request.getDescription(false));
+        ErrorResponse errorDetails = new ErrorResponse(new Date(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro interno do servidor",
+                ex.getMessage(), request.getDescription(false).replace("uri=", ""));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
