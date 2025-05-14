c1b9ff285375ad42dd17b10173165188120394d4
Jean Meneses Barros <jeanmeneses@hotmail.com>
2025-05-13 23:11:46 -0300

    fix: corrige importação da anotação RequestBody
    Corrige a importação da anotação RequestBody para utilizar a versão do framework Spring, resolvendo um possível conflito ou problema de incompatibilidade.

--- src/main/java/com/tokseg/armariointeligente/controllers/RetiradaController.java
+++ src/main/java/com/tokseg/armariointeligente/controllers/RetiradaController.java
@@ -2,7 +2,7 @@ package com.tokseg.armariointeligente.controllers;
 
 import com.tokseg.armariointeligente.models.retirada.Retirada;
 import com.tokseg.armariointeligente.services.RetiradaService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +19,7 @@ public class RetiradaController {
 
     @PostMapping
     public ResponseEntity<Retirada> registrar(@RequestBody Retirada retirada) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(retiradaService.registrarRetirada(retirada));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(retiradaService.registrarRetirada(retirada));
     }
 }
